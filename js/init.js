639c0ed97efd1067092797b39d384440bcc53536
Felix Chan <felixchan29@gmail.com>
2019-12-17 16:54:54 -0500

    hours

--- js/init.js
+++ js/init.js
@@ -1,12 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
   var API_KEY = "32687440704c232d18b7a627d8419872";
   var CORS_WRAPPER = "https://cors-anywhere.herokuapp.com/"
-  var URL = CORS_WRAPPER + "http://api.wunderground.com/api/" + API_KEY + "/hourly/q/NY/New_York.json";
+  var URL = CORS_WRAPPER + "https://api.darksky.net/forecast/" + API_KEY + "/40.7127,-74.0059?exclude=currently?exclude=minutely?exclude=daily%27";
   var ctx = document.getElementById("NYCWeatherChart").getContext("2d")
 
+  console.log(ctx)
 
-  makeRequest(URL, function(json) {
-    var data = generateDataSet(getHours(json), getFahrenheits(json));
-    var tempChart = new Chart(ctx).Line(data, { bezierCurve: true });
-  });
+  makeRequest(URL, ctx)
+  
 });
--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,9 +1,11 @@
 function getFahrenheits(result){
   // Your code goes here
+
 }
 
 function getHours(result){
   // Your code goes here
+  console.log("hours", result)
 }
 
 function generateDataSet(labels, data) {
@@ -11,5 +13,32 @@ function generateDataSet(labels, data) {
 }
 
 function makeRequest(endpoint, success) {
-  // Your code goes here
+  // Your code goes 
+  return fetch(endpoint)
+    .then(res => res.json())
+      .then(ele => {
+    console.log(ele)
+  })
 }
+
+// {
+//   var data = generateDataSet(getHours(json), getFahrenheits(json));
+//   // var tempChart = new Chart(ctx).Line(data, { bezierCurve: true });
+//   const chart = new Chart(ctx, {
+//     // The type of chart we want to create
+//     type: "line",
+ 
+//     // The data for our dataset
+//     data: {
+//         labels: ["January", "February", "March", "April", "May", "June", "July"],
+//         datasets: [{
+//             label: "My First dataset",
+//             data: [0, 10, 5, 2, 20, 30, 45],
+//         }]
+//     },
+ 
+//     // Configuration options go here
+//     options: {}
+//   });
+//   console.log(chart)
+// });
